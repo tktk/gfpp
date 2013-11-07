dc50d84651f58b95c76af1b12e964f0a3f6ac361
tekuto <g.tekuto@gmail.com>
2013-11-08 04:26:16 +0900

    以下の関数の宣言を追加 ・gf::fromString() ・gf::toString() ・gf::toUtf8() ・gf::toUtf16() ・gf::toUtf32()

---
+++ inc/gf4cpp/common/stringconverter.h
@@ -0,0 +1,69 @@
+﻿#ifndef GF4CPP_COMMON_STRINGCONVERTER_H
+#define GF4CPP_COMMON_STRINGCONVERTER_H
+
+#include "gf4cpp/common/primitives.h"
+#include "gf/util/import.h"
+
+namespace gf {
+    GFEXPORT Bool fromString(
+        Utf8 &
+        , const String &
+    );
+
+    GFEXPORT Bool fromString(
+        Utf16 &
+        , const String &
+    );
+
+    GFEXPORT Bool fromString(
+        Utf32 &
+        , const String &
+    );
+
+    GFEXPORT Bool toString(
+        String &
+        , const Utf8 &
+    );
+
+    GFEXPORT Bool toString(
+        String &
+        , const Utf16 &
+    );
+
+    GFEXPORT Bool toString(
+        String &
+        , const Utf32 &
+    );
+
+    GFEXPORT Bool toUtf8(
+        Utf8 &
+        , const Utf16 &
+    );
+
+    GFEXPORT Bool toUtf8(
+        Utf8 &
+        , const Utf32 &
+    );
+
+    GFEXPORT Bool toUtf16(
+        Utf16 &
+        , const Utf8 &
+    );
+
+    GFEXPORT Bool toUtf16(
+        Utf16 &
+        , const Utf32 &
+    );
+
+    GFEXPORT Bool toUtf32(
+        Utf32 &
+        , const Utf8 &
+    );
+
+    GFEXPORT Bool toUtf32(
+        Utf32 &
+        , const Utf16 &
+    );
+}
+
+#endif  // GF4CPP_COMMON_STRINGCONVERTER_H
