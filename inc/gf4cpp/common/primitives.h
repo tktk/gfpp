9d3ae3799948150dbb35d1c026536633111e60e8
tekuto <g.tekuto@gmail.com>
2013-11-08 04:15:32 +0900

    プロジェクト作成 以下の型を追加 ・gf::Bool ・gf::Size ・gf::Byte ・gf::UByte ・gf::Short ・gf::UShort ・gf::Int ・gf::UInt ・gf::Long ・gf::ULong ・gf::Float ・gf::Double ・gf::StringChar ・gf::Utf8Char ・gf::Utf16Char ・gf::Utf32Char ・gf::String ・gf::Utf8 ・gf::Utf16 ・gf::Utf32

---
+++ inc/gf4cpp/common/primitives.h
@@ -0,0 +1,36 @@
+﻿#ifndef GF4CPP_COMMON_PRIMITIVES_H
+#define GF4CPP_COMMON_PRIMITIVES_H
+
+#include "gf/common/primitives.h"
+
+#include <string>
+
+namespace gf {
+    typedef gfBool Bool;
+
+    typedef gfSize Size;
+
+    typedef gfByte Byte;
+    typedef gfUByte UByte;
+    typedef gfShort Short;
+    typedef gfUShort UShort;
+    typedef gfInt Int;
+    typedef gfUInt UInt;
+    typedef gfLong Long;
+    typedef gfULong ULong;
+
+    typedef gfFloat Float;
+    typedef gfDouble Double;
+
+    typedef gfStringChar StringChar;
+    typedef gfUtf8Char Utf8Char;
+    typedef gfUtf16Char Utf16Char;
+    typedef gfUtf32Char Utf32Char;
+
+    typedef std::basic_string< StringChar > String;
+    typedef std::basic_string< Utf8Char > Utf8;
+    typedef std::basic_string< Utf16Char > Utf16;
+    typedef std::basic_string< Utf32Char > Utf32;
+}
+
+#endif  // GF4CPP_COMMON_PRIMITIVES_H
