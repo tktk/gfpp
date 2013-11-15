8007d695715b10a769bb7302cf38bed475cc251a
tekuto <g.tekuto@gmail.com>
2013-11-15 18:32:51 +0900

    以下のインライン関数を追加 ・gf::setMain() ・gf::setMainPath()

--- inc/gf4cpp/boot/gameconfig.h
+++ inc/gf4cpp/boot/gameconfig.h
@@ -2,7 +2,9 @@
 #define GF4CPP_BOOT_GAMECONFIG_H
 
 #include "gf/boot/gameconfig.h"
+#include "gf4cpp/string/utf32.h"
 #include "gf4cpp/common/functional.h"
+#include "gf4cpp/common/primitives.h"
 
 namespace gf {
     typedef GfGameConfig GameConfig;
@@ -31,6 +33,28 @@ namespace gf {
     )
     {
         gfFree( _gameConfig );
+    }
+
+    inline gf::Bool setMain(
+        GameConfig &        _gameConfig
+        , const gf::Utf32 & _MAIN
+    )
+    {
+        return gfGameConfigSetMain(
+            &_gameConfig
+            , &_MAIN
+        );
+    }
+
+    inline gf::Bool setMainPath(
+        GameConfig &        _gameConfig
+        , const gf::Utf32 & _MAIN_PATH
+    )
+    {
+        return gfGameConfigSetMainPath(
+            &_gameConfig
+            , &_MAIN_PATH
+        );
     }
 }
 
