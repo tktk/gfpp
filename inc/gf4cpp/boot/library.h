7b2b4acd4329339b5e8843d466c7b8af30be192c
tekuto <g.tekuto@gmail.com>
2013-11-17 04:09:06 +0900

    以下の型を追加 ・gf::Library 以下のテンプレートインライン関数を追加 ・gfFree() 以下のインライン関数を追加 ・gf::newLibrary() ・gf::free() ・gf::getAddress()

---
+++ inc/gf4cpp/boot/library.h
@@ -0,0 +1,54 @@
+﻿#ifndef GF4CPP_BOOT_LIBRARY_H
+#define GF4CPP_BOOT_LIBRARY_H
+
+#include "gf/boot/library.h"
+#include "gf4cpp/string/utf32.h"
+
+namespace gf {
+    typedef GfLibrary Library;
+}
+
+template<>
+inline void gfFree(
+    gf::Library &   _library
+)
+{
+    gfLibraryFree(
+        &_library
+    );
+}
+
+namespace gf {
+    inline Library * newLibrary(
+        const Utf32 &   _FILE
+        , const Utf32 & _DIR
+    )
+    {
+        return gfLibraryNew(
+            &_FILE
+            , &_DIR
+        );
+    }
+
+    inline void free(
+        Library &   _library
+    )
+    {
+        gfFree(
+            _library
+        );
+    }
+
+    inline void * getAddress(
+        Library &       _library
+        , const Utf32 & _SYMBOL_NAME
+    )
+    {
+        return gfLibraryGetAddress(
+            &_library
+            , &_SYMBOL_NAME
+        );
+    }
+}
+
+#endif  // GF4CPP_BOOT_LIBRARY_H
