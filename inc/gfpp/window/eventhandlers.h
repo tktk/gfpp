d761b237535a08121ba4bae5a970f6ea20ec0fce
tekuto <g.tekuto@gmail.com>
2013-11-19 21:44:05 +0900

    以下の構造体の宣言を追加 ・gf::Window ・gf::WindowEventHandlers 以下の型を追加 ・gf::WindowCloseEventHandler ・gf::WindowPositionEventHandler ・gf::WindowSizeEventHandler ・gf::WindowPaintEventHandler 以下の関数の宣言を追加 ・gf::newWindow() ・gf::free() ・gf::close() ・gf::setTitle() ・gf::setPosition() ・gf::setSize() ・gf::repaint() ・gf::newWindowEventHandlers() ・gf::clone() ・gf::free() ・gf::setCloseEventHandler() ・gf::setPositionEventHandler() ・gf::setSizeEventHandler() ・gf::setPaintEventHandler()

---
+++ inc/gfpp/window/eventhandlers.h
@@ -0,0 +1,82 @@
+﻿#ifndef GFPP_WINDOW_EVENTHANDLERS_H
+#define GFPP_WINDOW_EVENTHANDLERS_H
+
+namespace gf {
+    struct WindowEventHandlers;
+}
+
+#include "gfpp/window/window.h"
+#include "gfpp/common/primitives.h"
+
+#include <functional>
+
+namespace gf {
+    typedef std::function<
+        void(
+            Window &
+        )
+    > WindowCloseEventHandler;
+
+    typedef std::function<
+        void(
+            Window &
+            , Int
+            , Int
+        )
+    > WindowPositionEventHandler;
+
+    typedef std::function<
+        void(
+            Window &
+            , Int
+            , Int
+        )
+    > WindowSizeEventHandler;
+
+    typedef std::function<
+        void(
+            Window &
+            , Int
+            , Int
+            , Int
+            , Int
+        )
+    > WindowPaintEventHandler;
+}
+
+#include "gfpp/util/import.h"
+
+namespace gf {
+    GFPPEXPORT WindowEventHandlers * newWindowEventHandlers(
+    );
+
+    GFPPEXPORT WindowEventHandlers * clone(
+        const WindowEventHandlers &
+    );
+
+    GFPPEXPORT void free(
+        WindowEventHandlers &
+    );
+
+    GFPPEXPORT void setCloseEventHandler(
+        WindowEventHandlers &
+        , const WindowCloseEventHandler &
+    );
+
+    GFPPEXPORT void setPositionEventHandler(
+        WindowEventHandlers &
+        , const WindowPositionEventHandler &
+    );
+
+    GFPPEXPORT void setSizeEventHandler(
+        WindowEventHandlers &
+        , const WindowSizeEventHandler &
+    );
+
+    GFPPEXPORT void setPaintEventHandler(
+        WindowEventHandlers &
+        , const WindowPaintEventHandler &
+    );
+}
+
+#endif  // GFPP_WINDOW_EVENTHANDLERS_H
---
+++ inc/gfpp/window/window.h
@@ -0,0 +1,59 @@
+﻿#ifndef GFPP_WINDOW_WINDOW_H
+#define GFPP_WINDOW_WINDOW_H
+
+namespace gf {
+    struct Window;
+}
+
+#include "gfpp/window/eventhandlers.h"
+#include "gfpp/string/utf32.h"
+#include "gfpp/common/primitives.h"
+#include "gfpp/util/import.h"
+
+namespace gf {
+    GFPPEXPORT Window * newWindow(
+        const WindowEventHandlers &
+        , const Utf32 &
+        , Int
+        , Int
+    );
+
+    GFPPEXPORT void free(
+        Window &
+    );
+
+    GFPPEXPORT void close(
+        Window &
+    );
+
+    GFPPEXPORT Bool setTitle(
+        Window &
+        , const Utf32 &
+    );
+
+    GFPPEXPORT void setPosition(
+        Window &
+        , Int
+        , Int
+    );
+
+    GFPPEXPORT void setSize(
+        Window &
+        , Int
+        , Int
+    );
+
+    GFPPEXPORT void repaint(
+        Window &
+        , Int
+        , Int
+        , Int
+        , Int
+    );
+
+    GFPPEXPORT void repaint(
+        Window &
+    );
+}
+
+#endif  // GFPP_WINDOW_WINDOW_H
