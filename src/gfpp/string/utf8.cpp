b07f5dd66df2b089006d1bce3df7ec6e434359b8
tekuto <g.tekuto@gmail.com>
2013-11-22 22:32:26 +0900

    以下の関数を追加 ・gf::newUtf8() ・gf::clone() ・gf::newUtf8FromString() ・gf::newUtf8FromUnicode() ・gf::free() ・gf::getPtr() ・gf::getLength() ・gf::assign() ・gf::assignFromString() ・gf::assignFromUnicode()

---
+++ src/gfpp/string/utf8.cpp
@@ -0,0 +1,223 @@
+﻿#include "gfpp/util/export.h"
+#include "gfpp/string/utf8.h"
+
+#include "gfpp/string/string.h"
+#include "gfpp/string/utf16.h"
+#include "gfpp/string/utf32.h"
+#include "gfpp/common/primitives.h"
+
+namespace gf {
+    Utf8 * newUtf8(
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * clone(
+        const Utf8 &
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8(
+        const Utf8Char *
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8(
+        const Utf8Char *
+        , Size
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromString(
+        const String &
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromString(
+        const StringChar *
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromString(
+        const StringChar *
+        , Size
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromUnicode(
+        const Utf16 &
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromUnicode(
+        const Utf16Char *
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromUnicode(
+        const Utf16Char *
+        , Size
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromUnicode(
+        const Utf32 &
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromUnicode(
+        const Utf32Char *
+    )
+    {
+        return nullptr;
+    }
+
+    Utf8 * newUtf8FromUnicode(
+        const Utf32Char *
+        , Size
+    )
+    {
+        return nullptr;
+    }
+
+    void free(
+        Utf8 &
+    )
+    {
+    }
+
+    const Utf8Char * getPtr(
+        const Utf8 &
+    )
+    {
+        return nullptr;
+    }
+
+    Size getLength(
+        const Utf8 &
+    )
+    {
+        return 0;
+    }
+
+    Bool assign(
+        Utf8 &
+        , const Utf8 &
+    )
+    {
+        return false;
+    }
+
+    Bool assign(
+        Utf8 &
+        , const Utf8Char *
+    )
+    {
+        return false;
+    }
+
+    Bool assign(
+        Utf8 &
+        , const Utf8Char *
+        , Size
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromString(
+        Utf8 &
+        , const String &
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromString(
+        Utf8 &
+        , const StringChar *
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromString(
+        Utf8 &
+        , const StringChar *
+        , Size
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromUnicode(
+        Utf8 &
+        , const Utf16 &
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromUnicode(
+        Utf8 &
+        , const Utf16Char *
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromUnicode(
+        Utf8 &
+        , const Utf16Char *
+        , Size
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromUnicode(
+        Utf8 &
+        , const Utf32 &
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromUnicode(
+        Utf8 &
+        , const Utf32Char *
+    )
+    {
+        return false;
+    }
+
+    Bool assignFromUnicode(
+        Utf8 &
+        , const Utf32Char *
+        , Size
+    )
+    {
+        return false;
+    }
+}
--- wscripts/libraries/string.py
+++ wscripts/libraries/string.py
@@ -12,6 +12,7 @@ def build( _context ):
             common.GFPP : {
                 MODULE_NAME : {
                     'string.cpp',
+                    'utf8.cpp',
                 },
             },
         },
