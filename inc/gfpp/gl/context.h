098b102235c3174f9eb8e284fa4a1df964d6a8ac
tekuto <g.tekuto@gmail.com>
2013-11-25 22:59:41 +0900

    以下の構造体の宣言を追加 ・gf::GLContext 以下の関数の宣言を追加 ・gf::newGLContext() ・gf::free() ・gf::glMakeCurrent()

---
+++ inc/gfpp/gl/context.h
@@ -0,0 +1,31 @@
+﻿#ifndef GFPP_GL_CONTEXT_H
+#define GFPP_GL_CONTEXT_H
+
+namespace gf {
+    struct GLContext;
+}
+
+#include "gfpp/gl/contextinfo.h"
+#include "gfpp/window/window.h"
+#include "gfpp/common/primitives.h"
+#include "gfpp/util/import.h"
+
+namespace gf {
+    GFPPEXPORT GLContext * newGLContext(
+        const GLContextInfo &
+    );
+
+    GFPPEXPORT void free(
+        GLContext &
+    );
+
+    GFPPEXPORT Bool glMakeCurrent(
+        GLContext &
+        , Window &
+    );
+
+    GFPPEXPORT void glMakeCurrent(
+    );
+}
+
+#endif  // GFPP_GL_CONTEXT_H
