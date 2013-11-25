4bf98bf3b0c2d96d07d3384fc6103bc67a79bcfa
tekuto <g.tekuto@gmail.com>
2013-11-25 22:55:41 +0900

    gfpp-glの生成ルールを追加 以下の関数を追加 ・gf::glSwapBuffers()

---
+++ src/gfpp/gl/gl.cpp
@@ -0,0 +1,12 @@
+﻿#include "gfpp/util/export.h"
+#include "gfpp/gl/gl.h"
+
+#include "gfpp/window/window.h"
+
+namespace gf {
+    void glSwapBuffers(
+        Window &
+    )
+    {
+    }
+}
--- wscripts/libraries/__init__.py
+++ wscripts/libraries/__init__.py
@@ -3,8 +3,10 @@
 from . import string
 from . import boot
 from . import window
+from . import gl
 
 def build( _context ):
     string.build( _context )
     boot.build( _context )
     window.build( _context )
+    gl.build( _context )
