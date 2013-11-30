8b82d7480c9c10d6d87ece1f431beacb8cfd9b90
tekuto <g.tekuto@gmail.com>
2013-11-30 16:13:49 +0900

    以下のテンプレート型を追加 ・gf::Unique ・gf::Shared ・gf::Weak 以下のテンプレート関数を追加 ・gf::unique() ・gf::shared() ・gf::weak() 以下のテンプレートファンクタを追加 ・gf::Free() ・gf::Less()

---
+++ inc/gfpp/common/functional.h
@@ -0,0 +1,37 @@
+﻿#ifndef GFPP_COMMON_FUNCTIONAL_H
+#define GFPP_COMMON_FUNCTIONAL_H
+
+#include "fgpp/common/primitives.h"
+
+namespace gf {
+    template< typename T >
+    struct Free
+    {
+        void operator()(
+            T   _obj
+        ) const
+        {
+            free( *_obj );
+        }
+    };
+
+    template<
+        typename KEY1_T
+        , typename KEY2_T
+    >
+    struct Less
+    {
+        fg::Bool operator()(
+            const KEY1_T &      _KEY1
+            , const KEY2_T &    _KEY2
+        ) const
+        {
+            return less(
+                *_KEY1
+                , *_KEY2
+            );
+        }
+    };
+}
+
+#endif  // GFPP_COMMON_FUNCTIONAL_H
