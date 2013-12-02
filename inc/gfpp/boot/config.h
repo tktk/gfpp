7403a5dca00195f6d715915552657127b39ca870
tekuto <g.tekuto@gmail.com>
2013-12-02 17:26:57 +0900

    以下の関数の宣言を追加 ・gf::existsGLDoubleBuffer() ・gf::getGLDoubleBuffer() ・gf::setGLDoubleBuffer() ・gf::clearGLDoubleBuffer() ・gf::existsGLAuxBuffers() ・gf::getGLAuxBuffers() ・gf::setGLAuxBuffers() ・gf::clearGLAuxBuffers() ・gf::existsGLRedBits() ・gf::getGLRedBits() ・gf::setGLRedBits() ・gf::clearGLRedBits() ・gf::existsGLGreenBits() ・gf::getGLGreenBits() ・gf::setGLGreenBits() ・gf::clearGLGreenBits() ・gf::existsGLBlueBits() ・gf::getGLBlueBits() ・gf::setGLBlueBits() ・gf::clearGLBlueBits() ・gf::existsGLAlphaBits() ・gf::getGLAlphaBits() ・gf::setGLAlphaBits() ・gf::clearGLAlphaBits() ・gf::existsGLDepthBits() ・gf::getGLDepthBits() ・gf::setGLDepthBits() ・gf::clearGLDepthBits() ・gf::existsGLStencilBits() ・gf::getGLStencilBits() ・gf::setGLStencilBits() ・gf::clearGLStencilBits() ・gf::existsGLAccumRedBits() ・gf::getGLAccumRedBits() ・gf::setGLAccumRedBits() ・gf::clearGLAccumRedBits() ・gf::existsGLAccumGreenBits() ・gf::getGLAccumGreenBits() ・gf::setGLAccumGreenBits() ・gf::clearGLAccumGreenBits() ・gf::existsGLAccumBlueBits() ・gf::getGLAccumBlueBits() ・gf::setGLAccumBlueBits() ・gf::clearGLAccumBlueBits() ・gf::existsGLAccumAlphaBits() ・gf::getGLAccumAlphaBits() ・gf::setGLAccumAlphaBits() ・gf::clearGLAccumAlphaBits()

--- inc/gfpp/boot/config.h
+++ inc/gfpp/boot/config.h
@@ -33,6 +33,210 @@ namespace gf {
     FGPPEXPORT void clearBinDir(
         BootConfig &
     );
+
+    FGPPEXPORT fg::Bool existsGLDoubleBuffer(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool getGLDoubleBuffer(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLDoubleBuffer(
+        BootConfig &
+        , fg::Bool
+    );
+
+    FGPPEXPORT void clearGLDoubleBuffer(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLAuxBuffers(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLAuxBuffers(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLAuxBuffers(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLAuxBuffers(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLRedBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLRedBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLRedBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLRedBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLGreenBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLGreenBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLGreenBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLGreenBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLBlueBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLBlueBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLBlueBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLBlueBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLAlphaBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLAlphaBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLAlphaBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLAlphaBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLDepthBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLDepthBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLDepthBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLDepthBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLStencilBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLStencilBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLStencilBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLStencilBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLAccumRedBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLAccumRedBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLAccumRedBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLAccumRedBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLAccumGreenBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLAccumGreenBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLAccumGreenBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLAccumGreenBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLAccumBlueBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLAccumBlueBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLAccumBlueBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLAccumBlueBits(
+        BootConfig &
+    );
+
+    FGPPEXPORT fg::Bool existsGLAccumAlphaBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT fg::Int getGLAccumAlphaBits(
+        const BootConfig &
+    );
+
+    FGPPEXPORT void setGLAccumAlphaBits(
+        BootConfig &
+        , fg::Int
+    );
+
+    FGPPEXPORT void clearGLAccumAlphaBits(
+        BootConfig &
+    );
 }
 
 #endif  // GFPP_BOOT_CONFIG_H
