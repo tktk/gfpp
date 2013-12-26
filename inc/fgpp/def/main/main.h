fb11f4b16085fb87729a8f14c91d8d0136be5d86
tekuto <g.tekuto@gmail.com>
2013-12-26 16:59:11 +0900

    fg::MainProcの宣言を別のファイルに分離

---
+++ inc/fgpp/def/main/main.h
@@ -0,0 +1,13 @@
+﻿#ifndef FGPP_DEF_MAIN_MAIN_H
+#define FGPP_DEF_MAIN_MAIN_H
+
+#include "fgpp/def/main/args.h"
+#include "fgpp/def/common/primitives.h"
+
+namespace fg {
+    typedef Int ( * MainProc )(
+        const Args &
+    );
+}
+
+#endif  // FGPP_DEF_MAIN_MAIN_H
