8aec120bf23fb29c46b7d9443b20b27a13e0cef7
tekuto <g.tekuto@gmail.com>
2013-12-26 17:19:57 +0900

    以下の関数の宣言、ダミー関数定義をマクロで生成するように修正 ・gf::boot()

--- inc/gfpp/main/boot.h
+++ inc/gfpp/main/boot.h
@@ -1,17 +1,19 @@
 ﻿#ifndef GFPP_MAIN_BOOT_H
 #define GFPP_MAIN_BOOT_H
 
-#include "gfpp/main/bootconfig.h"
-#include "fgpp/string/utf32.h"
-#include "fgpp/common/primitives.h"
-#include "fgpp/util/import.h"
+#include "gfpp/def/main/bootconfig.h"
+#include "fgpp/def/string/utf32.h"
+#include "fgpp/def/common/primitives.h"
+#include "fgpp/util/import_new.h"
 
 namespace gf {
-    FGPPEXPORT fg::Int boot(
-        const fg::Utf32 &
-        , const fg::Utf32 &
-        , const BootConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        fg::Int boot(
+            const fg::Utf32 &
+            , const fg::Utf32 &
+            , const BootConfig &
+        )
+    )
 }
 
 #endif  // GFPP_MAIN_BOOT_H
--- src/gfpp/main/boot.cpp
+++ src/gfpp/main/boot.cpp
@@ -1,17 +1,2 @@
-﻿#include "fgpp/util/export.h"
+﻿#include "fgpp/util/export_new.h"
 #include "gfpp/main/boot.h"
-
-#include "gfpp/main/bootconfig.h"
-#include "fgpp/string/utf32.h"
-#include "fgpp/common/primitives.h"
-
-namespace gf {
-    fg::Int boot(
-        const fg::Utf32 &
-        , const fg::Utf32 &
-        , const BootConfig &
-    )
-    {
-        return 0;
-    }
-}
