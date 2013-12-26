09807190c5090e633bad611216df5456af6bd0db
tekuto <g.tekuto@gmail.com>
2013-12-26 17:26:25 +0900

    fg::GLConfigに関係する関数の宣言、ダミー関数定義をマクロで生成するように修正 fg::GLConfigの宣言を別のファイルに分離

--- inc/fgpp/gl/config.h
+++ inc/fgpp/gl/config.h
@@ -1,110 +1,156 @@
 ﻿#ifndef FGPP_GL_CONFIG_H
 #define FGPP_GL_CONFIG_H
 
-namespace fg {
-    struct GLConfig;
-}
-
-#include "fgpp/gl/version.h"
-#include "fgpp/common/primitives.h"
-#include "fgpp/util/import.h"
+#include "fgpp/def/gl/config.h"
+#include "fgpp/def/gl/version.h"
+#include "fgpp/def/common/primitives.h"
+#include "fgpp/util/import_new.h"
 
 namespace fg {
-    FGPPEXPORT GLVersion getVersion(
-        const GLConfig &
-    );
+    FGPP_FUNCTION(
+        GLVersion getVersion(
+            const GLConfig &
+        )
+        , return GL_VERSION_INVALID;
+    )
 
-    FGPPEXPORT Bool getDoubleBuffer(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool getDoubleBuffer(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsAuxBuffers(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsAuxBuffers(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getAuxBuffers(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getAuxBuffers(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsRedBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsRedBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getRedBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getRedBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsGreenBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsGreenBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getGreenBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getGreenBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsBlueBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsBlueBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getBlueBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getBlueBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsAlphaBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsAlphaBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getAlphaBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getAlphaBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsDepthBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsDepthBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getDepthBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getDepthBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsStencilBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsStencilBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getStencilBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getStencilBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsAccumRedBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsAccumRedBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getAccumRedBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getAccumRedBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsAccumGreenBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsAccumGreenBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getAccumGreenBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getAccumGreenBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsAccumBlueBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsAccumBlueBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getAccumBlueBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getAccumBlueBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Bool existsAccumAlphaBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool existsAccumAlphaBits(
+            const GLConfig &
+        )
+    )
 
-    FGPPEXPORT Int getAccumAlphaBits(
-        const GLConfig &
-    );
+    FGPP_FUNCTION_NUM(
+        Int getAccumAlphaBits(
+            const GLConfig &
+        )
+    )
 }
 
 #endif  // FGPP_GL_CONFIG_H
--- inc/gfpp/gl/config.h
+++ inc/gfpp/gl/config.h
@@ -1,127 +1,179 @@
 ﻿#ifndef GFPP_GL_CONFIG_H
 #define GFPP_GL_CONFIG_H
 
-#include "fgpp/gl/config.h"
-#include "fgpp/gl/version.h"
-#include "fgpp/common/primitives.h"
-#include "fgpp/util/import.h"
+#include "fgpp/def/gl/config.h"
+#include "fgpp/def/gl/version.h"
+#include "fgpp/def/common/primitives.h"
+#include "fgpp/util/import_new.h"
 
 namespace gf {
-    FGPPEXPORT fg::GLConfig * newGLConfig(
-    );
+    FGPP_FUNCTION_PTR(
+        fg::GLConfig * newGLConfig(
+        )
+    )
 
-    FGPPEXPORT void free(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void free(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setVersion(
-        fg::GLConfig &
-        , fg::GLVersion
-    );
+    FGPP_FUNCTION_VOID(
+        void setVersion(
+            fg::GLConfig &
+            , fg::GLVersion
+        )
+    )
 
-    FGPPEXPORT void setDoubleBuffer(
-        fg::GLConfig &
-        , fg::Bool
-    );
+    FGPP_FUNCTION_VOID(
+        void setDoubleBuffer(
+            fg::GLConfig &
+            , fg::Bool
+        )
+    )
 
-    FGPPEXPORT void setAuxBuffers(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setAuxBuffers(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearAuxBuffers(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearAuxBuffers(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setRedBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setRedBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearRedBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearRedBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setGreenBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setGreenBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearGreenBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearGreenBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setBlueBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setBlueBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearBlueBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearBlueBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setAlphaBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setAlphaBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearAlphaBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearAlphaBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setDepthBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setDepthBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearDepthBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearDepthBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setStencilBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setStencilBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearStencilBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearStencilBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setAccumRedBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setAccumRedBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearAccumRedBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearAccumRedBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setAccumGreenBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setAccumGreenBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearAccumGreenBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearAccumGreenBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setAccumBlueBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setAccumBlueBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearAccumBlueBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearAccumBlueBits(
+            fg::GLConfig &
+        )
+    )
 
-    FGPPEXPORT void setAccumAlphaBits(
-        fg::GLConfig &
-        , fg::Int
-    );
+    FGPP_FUNCTION_VOID(
+        void setAccumAlphaBits(
+            fg::GLConfig &
+            , fg::Int
+        )
+    )
 
-    FGPPEXPORT void clearAccumAlphaBits(
-        fg::GLConfig &
-    );
+    FGPP_FUNCTION_VOID(
+        void clearAccumAlphaBits(
+            fg::GLConfig &
+        )
+    )
 }
 
 #endif  // GFPP_GL_CONFIG_H
