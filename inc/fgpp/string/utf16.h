7cc2fbdb93d5e82dcd629783c665f7e671f1f89b
tekuto <g.tekuto@gmail.com>
2013-12-26 14:49:24 +0900

    fg::Utf16に関係する関数の宣言、ダミー関数定義をマクロで生成するように修正 fg::Utf16の宣言を別のファイルに分離

--- inc/fgpp/string/utf16.h
+++ inc/fgpp/string/utf16.h
@@ -1,201 +1,274 @@
 ﻿#ifndef FGPP_STRING_UTF16_H
 #define FGPP_STRING_UTF16_H
 
-namespace fg {
-    struct Utf16;
-}
-
-#include "fgpp/string/string.h"
-#include "fgpp/string/utf8.h"
-#include "fgpp/string/utf32.h"
-#include "fgpp/common/primitives.h"
-#include "fgpp/util/import.h"
+#include "fgpp/def/string/utf16.h"
+#include "fgpp/def/string/string.h"
+#include "fgpp/def/string/utf8.h"
+#include "fgpp/def/string/utf32.h"
+#include "fgpp/def/common/primitives.h"
+#include "fgpp/util/import_new.h"
 
 namespace fg {
-    FGPPEXPORT Utf16 * newUtf16(
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16(
+        )
+    )
 
-    FGPPEXPORT Utf16 * clone(
-        const Utf16 &
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * clone(
+            const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16(
-        const Utf16Char *
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16(
+            const Utf16Char *
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16(
-        const Utf16Char *
-        , Size
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16(
+            const Utf16Char *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromString(
-        const String &
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromString(
+            const String &
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromString(
-        const StringChar *
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromString(
+            const StringChar *
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromString(
-        const StringChar *
-        , Size
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromString(
+            const StringChar *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromUnicode(
-        const Utf8 &
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromUnicode(
+            const Utf8 &
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromUnicode(
-        const Utf8Char *
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromUnicode(
+            const Utf8Char *
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromUnicode(
-        const Utf8Char *
-        , Size
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromUnicode(
+            const Utf8Char *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromUnicode(
-        const Utf32 &
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromUnicode(
+            const Utf32 &
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromUnicode(
-        const Utf32Char *
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromUnicode(
+            const Utf32Char *
+        )
+    )
 
-    FGPPEXPORT Utf16 * newUtf16FromUnicode(
-        const Utf32Char *
-        , Size
-    );
+    FGPP_FUNCTION_PTR(
+        Utf16 * newUtf16FromUnicode(
+            const Utf32Char *
+            , Size
+        )
+    )
 
-    FGPPEXPORT void free(
-        Utf16 &
-    );
+    FGPP_FUNCTION_VOID(
+        void free(
+            Utf16 &
+        )
+    )
 
-    FGPPEXPORT const Utf16Char * getPtr(
-        const Utf16 &
-    );
+    FGPP_FUNCTION_PTR(
+        const Utf16Char * getPtr(
+            const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Size getLength(
-        const Utf16 &
-    );
+    FGPP_FUNCTION_NUM(
+        Size getLength(
+            const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool assign(
-        Utf16 &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assign(
+            Utf16 &
+            , const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool assign(
-        Utf16 &
-        , const Utf16Char *
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assign(
+            Utf16 &
+            , const Utf16Char *
+        )
+    )
 
-    FGPPEXPORT Bool assign(
-        Utf16 &
-        , const Utf16Char *
-        , Size
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assign(
+            Utf16 &
+            , const Utf16Char *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Bool assignFromString(
-        Utf16 &
-        , const String &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromString(
+            Utf16 &
+            , const String &
+        )
+    )
 
-    FGPPEXPORT Bool assignFromString(
-        Utf16 &
-        , const StringChar *
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromString(
+            Utf16 &
+            , const StringChar *
+        )
+    )
 
-    FGPPEXPORT Bool assignFromString(
-        Utf16 &
-        , const StringChar *
-        , Size
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromString(
+            Utf16 &
+            , const StringChar *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Bool assignFromUnicode(
-        Utf16 &
-        , const Utf8 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromUnicode(
+            Utf16 &
+            , const Utf8 &
+        )
+    )
 
-    FGPPEXPORT Bool assignFromUnicode(
-        Utf16 &
-        , const Utf8Char *
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromUnicode(
+            Utf16 &
+            , const Utf8Char *
+        )
+    )
 
-    FGPPEXPORT Bool assignFromUnicode(
-        Utf16 &
-        , const Utf8Char *
-        , Size
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromUnicode(
+            Utf16 &
+            , const Utf8Char *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Bool assignFromUnicode(
-        Utf16 &
-        , const Utf32 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromUnicode(
+            Utf16 &
+            , const Utf32 &
+        )
+    )
 
-    FGPPEXPORT Bool assignFromUnicode(
-        Utf16 &
-        , const Utf32Char *
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromUnicode(
+            Utf16 &
+            , const Utf32Char *
+        )
+    )
 
-    FGPPEXPORT Bool assignFromUnicode(
-        Utf16 &
-        , const Utf32Char *
-        , Size
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool assignFromUnicode(
+            Utf16 &
+            , const Utf32Char *
+            , Size
+        )
+    )
 
-    FGPPEXPORT Bool toInt(
-        Int &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toInt(
+            Int &
+            , const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool toInt(
-        Int &
-        , const Utf16 &
-        , Int
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toInt(
+            Int &
+            , const Utf16 &
+            , Int
+        )
+    )
 
-    FGPPEXPORT Bool toUInt(
-        UInt &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toUInt(
+            UInt &
+            , const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool toUInt(
-        UInt &
-        , const Utf16 &
-        , Int
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toUInt(
+            UInt &
+            , const Utf16 &
+            , Int
+        )
+    )
 
-    FGPPEXPORT Bool toLong(
-        Long &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toLong(
+            Long &
+            , const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool toLong(
-        Long &
-        , const Utf16 &
-        , Int
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toLong(
+            Long &
+            , const Utf16 &
+            , Int
+        )
+    )
 
-    FGPPEXPORT Bool toULong(
-        ULong &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toULong(
+            ULong &
+            , const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool toULong(
-        ULong &
-        , const Utf16 &
-        , Int
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toULong(
+            ULong &
+            , const Utf16 &
+            , Int
+        )
+    )
 
-    FGPPEXPORT Bool toFloat(
-        Float &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toFloat(
+            Float &
+            , const Utf16 &
+        )
+    )
 
-    FGPPEXPORT Bool toDouble(
-        Double &
-        , const Utf16 &
-    );
+    FGPP_FUNCTION_BOOL(
+        Bool toDouble(
+            Double &
+            , const Utf16 &
+        )
+    )
 }
 
 #endif  // FGPP_STRING_UTF16_H
