7b2a769c797310582eb57c8270b98f8de1b081d9
tekuto <g.tekuto@gmail.com>
2013-12-26 16:24:00 +0900

    fg::WindowEventHandlersに関係する関数の宣言、ダミー関数定義をマクロで生成するように修正 fg::WindowEventHandlersの宣言を別のファイルに分離

--- inc/fgpp/window/eventhandlers.h
+++ inc/fgpp/window/eventhandlers.h
@@ -1,100 +1,131 @@
 ﻿#ifndef FGPP_WINDOW_EVENTHANDLERS_H
 #define FGPP_WINDOW_EVENTHANDLERS_H
 
-namespace fg {
-    struct WindowEventHandlers;
-}
-
-#include "fgpp/window/closeevent.h"
-#include "fgpp/window/positionevent.h"
-#include "fgpp/window/sizeevent.h"
-#include "fgpp/window/paintevent.h"
-#include "fgpp/window/keyevent.h"
-#include "fgpp/window/mousebuttonevent.h"
-#include "fgpp/window/mousemotionevent.h"
-#include "fgpp/util/import.h"
+#include "fgpp/def/window/eventhandlers.h"
+#include "fgpp/def/window/closeevent.h"
+#include "fgpp/def/window/positionevent.h"
+#include "fgpp/def/window/sizeevent.h"
+#include "fgpp/def/window/paintevent.h"
+#include "fgpp/def/window/keyevent.h"
+#include "fgpp/def/window/mousebuttonevent.h"
+#include "fgpp/def/window/mousemotionevent.h"
+#include "fgpp/util/import_new.h"
 
 namespace fg {
-    FGPPEXPORT WindowEventHandlers * newWindowEventHandlers(
-    );
+    FGPP_FUNCTION_PTR(
+        WindowEventHandlers * newWindowEventHandlers(
+        )
+    )
 
-    FGPPEXPORT WindowEventHandlers * clone(
-        const WindowEventHandlers &
-    );
+    FGPP_FUNCTION_PTR(
+        WindowEventHandlers * clone(
+            const WindowEventHandlers &
+        )
+    )
 
-    FGPPEXPORT void free(
-        WindowEventHandlers &
-    );
+    FGPP_FUNCTION_VOID(
+        void free(
+            WindowEventHandlers &
+        )
+    )
 
-    FGPPEXPORT void setCloseEventHandler(
-        WindowEventHandlers &
-        , const WindowCloseEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setCloseEventHandler(
+            WindowEventHandlers &
+            , const WindowCloseEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callCloseEventHandler(
-        const WindowEventHandlers &
-        , const WindowCloseEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callCloseEventHandler(
+            const WindowEventHandlers &
+            , const WindowCloseEvent &
+        )
+    )
 
-    FGPPEXPORT void setPositionEventHandler(
-        WindowEventHandlers &
-        , const WindowPositionEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setPositionEventHandler(
+            WindowEventHandlers &
+            , const WindowPositionEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callPositionEventHandler(
-        const WindowEventHandlers &
-        , const WindowPositionEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callPositionEventHandler(
+            const WindowEventHandlers &
+            , const WindowPositionEvent &
+        )
+    )
 
-    FGPPEXPORT void setSizeEventHandler(
-        WindowEventHandlers &
-        , const WindowSizeEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setSizeEventHandler(
+            WindowEventHandlers &
+            , const WindowSizeEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callSizeEventHandler(
-        const WindowEventHandlers &
-        , const WindowSizeEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callSizeEventHandler(
+            const WindowEventHandlers &
+            , const WindowSizeEvent &
+        )
+    )
 
-    FGPPEXPORT void setPaintEventHandler(
-        WindowEventHandlers &
-        , const WindowPaintEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setPaintEventHandler(
+            WindowEventHandlers &
+            , const WindowPaintEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callPaintEventHandler(
-        const WindowEventHandlers &
-        , const WindowPaintEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callPaintEventHandler(
+            const WindowEventHandlers &
+            , const WindowPaintEvent &
+        )
+    )
 
-    FGPPEXPORT void setKeyEventHandler(
-        WindowEventHandlers &
-        , const WindowKeyEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setKeyEventHandler(
+            WindowEventHandlers &
+            , const WindowKeyEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callKeyEventHandler(
-        const WindowEventHandlers &
-        , const WindowKeyEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callKeyEventHandler(
+            const WindowEventHandlers &
+            , const WindowKeyEvent &
+        )
+    )
 
-    FGPPEXPORT void setMouseButtonEventHandler(
-        WindowEventHandlers &
-        , const WindowMouseButtonEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setMouseButtonEventHandler(
+            WindowEventHandlers &
+            , const WindowMouseButtonEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callMouseButtonEventHandler(
-        const WindowEventHandlers &
-        , const WindowMouseButtonEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callMouseButtonEventHandler(
+            const WindowEventHandlers &
+            , const WindowMouseButtonEvent &
+        )
+    )
 
-    FGPPEXPORT void setMouseMotionEventHandler(
-        WindowEventHandlers &
-        , const WindowMouseMotionEventHandler &
-    );
+    FGPP_FUNCTION_VOID(
+        void setMouseMotionEventHandler(
+            WindowEventHandlers &
+            , const WindowMouseMotionEventHandler &
+        )
+    )
 
-    FGPPEXPORT void callMouseMotionEventHandler(
-        const WindowEventHandlers &
-        , const WindowMouseMotionEvent &
-    );
+    FGPP_FUNCTION_VOID(
+        void callMouseMotionEventHandler(
+            const WindowEventHandlers &
+            , const WindowMouseMotionEvent &
+        )
+    )
 }
 
 #endif  // FGPP_WINDOW_EVENTHANDLERS_H
